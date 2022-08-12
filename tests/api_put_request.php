2a0d90c6ff20a4521b06e5378870a32cf88e7c84
Iaroslav Os <osmnbx@gmail.com>
2022-08-12 08:48:11 +0300

    added/updated

--- tests/api_put_request.php
+++ tests/api_put_request.php
@@ -4,10 +4,10 @@ header("Content-type:application/json");
 
 if ($_SERVER["REQUEST_METHOD"] === "PUT") {
 
-    parse_str(file_get_contents("php://input"), $put_data);
+    parse_str(file_get_contents("php://input"), $put_request_data);
 
     $result = [
-        "data" => $put_data,
+        "data" => $put_request_data,
     ];
 
     echo json_encode($result);
