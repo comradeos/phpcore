e6408a1cb91a792b1f7b7bea5dcb1558ca250c93
Iaroslav Os <osmnbx@gmail.com>
2023-09-08 12:38:59 +0300

    Create AnonymousClass.php

---
+++ php000abc/practice/AnonymousClass.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once "ToolBox.php";
+
+interface IMyClassA {
+    public static function MyClassFunc();
+}
+
+class TestAnonymousClass
+{
+    public static function MyFunction():void
+    {
+
+    }
+    public static function Run():void
+    {
+
+    }
+}
