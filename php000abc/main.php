dafa8a63cc2a9701666804834b126d2bdd514b09
Iaroslav Os <osmnbx@gmail.com>
2023-09-08 19:48:14 +0300

    updated

--- php000abc/main.php
+++ php000abc/main.php
@@ -7,6 +7,7 @@ require_once "practice/ManyFuncArgs.php";
 require_once "practice/ArraysInArray.php";
 require_once "practice/ReadFiles.php";
 require_once "practice/Generators.php";
+require_once "practice/AnonymousClass.php";
 
 TestTraits::Run();
 TestTextPackage::Run();
@@ -15,3 +16,4 @@ TestManyFuncArgs::Run();
 TestArraysInArray::Run();
 TestReadFiles::Run();
 TestGenerators::Run();
+TestAnonymousClass::Run();
--- php000abc/practice/AnonymousClass.php
+++ php000abc/practice/AnonymousClass.php
@@ -2,18 +2,23 @@
 
 require_once "ToolBox.php";
 
-interface IMyClassA {
-    public static function MyClassFunc();
-}
 
 class TestAnonymousClass
 {
-    public static function MyFunction():void
-    {
-
-    }
     public static function Run():void
     {
+        Tools::Line();
+        $a = new class {
+            public static int $id;
+            public static string $name;
+            public function __construct()
+            {
+                self::$id = 1;
+                self::$name = "MyName";
+            }
+        };
 
+        echo $a::$id . "\n";
+        echo $a::$name . "\n";
     }
 }
