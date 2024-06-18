586166cbecde52ae65cb9aa54ef9b090127e76db
Iaroslav Os <osmnbx@gmail.com>
2024-06-18 14:17:27 +0300

    updated

--- tests/php_word_header_font/index.php
+++ tests/php_word_header_font/index.php
@@ -1,3 +1,34 @@
 <?php
+require_once 'vendor/autoload.php';
 
-echo "Document successfully created!\n";
+use PhpOffice\PhpWord\PhpWord;
+use PhpOffice\PhpWord\SimpleType\Jc;
+use PhpOffice\PhpWord\Element\Field;
+
+// Создание нового документа
+$phpWord = new PhpWord();
+
+// Добавление раздела
+$section = $phpWord->addSection();
+
+
+// Определение стиля шрифта
+$fontStyle = [
+    'name' => 'Times New Roman',
+    'size' => 12,
+    'color' => '000000',
+    'bold' => true,
+    'italic' => true,
+];
+
+$paragraphStyle = [
+    'alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER
+];
+
+$header = $section->addHeader();    
+$header->addPreserveText('{PAGE}', $fontStyle, $paragraphStyle);
+
+$writer = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+$writer->save(__DIR__ . '/simpleDocument.docx');
+
+echo "Документ создан.";
